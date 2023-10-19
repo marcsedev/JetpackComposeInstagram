54675a3e00b9cf8fca57d544832520a8b662c7ac
Marc Sogas Esteve <marc.sogas@gmail.com>
2023-10-19 10:04:52 +0200

    Add Data and Retrofit

---
+++ app/src/main/java/com/marcsedev/jetpackcomposeinstagram/login/data/network/response/LoginResponse.kt
@@ -0,0 +1,5 @@
+package com.marcsedev.jetpackcomposeinstagram.login.data.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(@SerializedName("success") val success:Boolean)
